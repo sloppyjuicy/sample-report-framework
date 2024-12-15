f8fed4463d89e5e0de3f161ef9902ce0955c2657
Nandha Kumar <nandha@Nandhas-MacBook-Air.local>
2024-12-15 19:04:41 +0530

    test

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,6 @@ from flask import Flask
 # from flask_cors import CORS
 # from config import Config
 
-# def create_app(config_name):
-#     """
-#     Creates the application object
-#     """
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 from mongo import mongodb
